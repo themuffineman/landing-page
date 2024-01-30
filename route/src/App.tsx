b44770c291e772cc886f2e533f1b470376bf9140
Petrus <petrusheya@gmail.com>
2024-01-30 08:24:51 +0200

    Shared Layout

--- route/src/App.tsx
+++ route/src/App.tsx
@@ -4,7 +4,7 @@ import NavLinks from './utils/NavLinks'
 import Footer from './components/Footer'
 import FooterLinks from './utils/FooterLinks'
 import Home from './components/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom'
 import About from './components/About'
 import Features from './components/Features'
 import Pricing from './components/Pricing'
@@ -15,23 +15,39 @@ import NotFound from './components/NotFound'
 const App: React.FC = () => {
   return (
     <>
-      <BrowserRouter>
-        <NavBar links={NavLinks} />
+      <Router>
         
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/features' element={<Features/>}/>
-          <Route path='/pricing' element={<Pricing/>}/>
-          <Route path='/contact' element={<Contact/>}/>
+
+          <Route path='/' element={<SharedLayout/>}>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='features' element={<Features/>}/>
+            <Route path='pricing' element={<Pricing/>}/>
+            <Route path='contact' element={<Contact/>}/>
+          </Route>
+          
           <Route path="*" element={<NotFound/>}/>
+
         </Routes>
         
         <Footer FooterLinks={FooterLinks}/>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
+
+
+const SharedLayout = () => {
+
+  return(
+    <>
+        <NavBar links={NavLinks} />
+        <Outlet/>
+    </>
+  )
+}
+
 
 
 export default App
